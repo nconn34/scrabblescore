30d1f0ad9da1bc19781d05f96189395331eb55d3
Nathan Conn <nconn34@gmail.com>
2022-02-09 20:43:37 -0800

    Wrote three passing tests

--- ScrabbleScore.Tests/ScrabbleScoreTests/ModelTests.cs
+++ ScrabbleScore.Tests/ScrabbleScoreTests/ModelTests.cs
@@ -13,17 +13,49 @@ namespace Scrabble.Tests
     [TestMethod]
     public void Scrabble_ReturnsAScore()
     {
-    //   string word1 = "tin";
-    }
-    // //Act
-    // // double score = 3.33;
-    // //ScrabbleGame newGame = new ScrabbleGame();
-    // ScrabbleGame.Score(word1);
+    //Arrange
+    string word1 = "tin";
+  
+    //Act
+    double score = 3.33;
+    double wordScore = ScrabbleGame.Score(word1);
 
-    // //Assert
-    // Assert.AreEqual(newGame.Score(word1), score);
+    //Assert
+    Assert.AreEqual(wordScore, score);
     
+    }
+
+    [TestMethod]
+    public void Scrabble_ReturnsAScoreForCaps()
+    {
+    //Arrange
+    string word1 = "TIN";
+  
+    //Act
+    double score = 3.33;
+    double wordScore = ScrabbleGame.Score(word1);
+
+    //Assert
+    Assert.AreEqual(wordScore, score);
+    }
+
+    
+    [TestMethod]
+    public void Scrabble_ReturnsAScoreForCapsAndAllPoints()
+    {
+    //Arrange
+    string word1 = "ZXKFBDA";
+  
+    //Act
+    double score = 35.41;
+    double wordScore = ScrabbleGame.Score(word1);
+
+    //Assert
+    Assert.AreEqual(wordScore, score);
+    }
   }
 }
+
+
 
 
